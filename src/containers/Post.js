c9beb26edd35de77e40505b2cced9292568680bb
Crysp <antiprogs@gmail.com>
2018-04-20 14:56:37 +0300

    Композиция запроса и мутации в компоненте поста

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { compose, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 
 class Post extends React.Component {
+    onVote = () => {
+        const { votePost, getPostData } = this.props;
+        votePost().then(() => {
+            getPostData.refetch();
+        });
+    };
     render() {
-        const { match: { params: { id } } } = this.props;
-        return (
+        const {
+            getPostData: { loading, post },
+            votePost: { loading: voteLoading }
+        } = this.props;
+        return loading ? 'Loading...' : (
             <div>
-                <h1>#{id} Post {id}</h1>
-                <button className="vote"> Votes: 0</button>
+                <h1>#{post.id} {post.title}</h1>
+                <button
+                    className="vote"
+                    onClick={this.onVote}
+                    disabled={voteLoading}
+                >
+                    Votes: {post.votes}
+                </button>
             </div>
         );
     }
 }
 
 
-export default withRouter(Post);
+export default withRouter(compose(
+    graphql(gql`
+        query getPost($id: Int!) {
+            post(id: $id) {
+                id
+                title
+                votes
+            }
+        }
+    `, {
+        name: 'getPostData',
+        options: ({ match: { params: { id } } }) => ({
+            variables: {
+                id
+            }
+        })
+    }),
+    graphql(gql`
+        mutation vote($id: Int!) {
+            upvotePost(postId: $id) {
+                id
+            }
+        }
+    `, {
+        name: 'votePost',
+        options: ({ match: { params: { id } } }) => ({
+            variables: {
+                id
+            }
+        })
+    })
+)(Post))
