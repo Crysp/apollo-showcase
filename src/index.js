0bcc8b7af08ab6ebd5fa0d0c20cd64cee2f146d4
Crysp <antiprogs@gmail.com>
2018-04-20 14:11:52 +0300

    Подключение Apollo клиента к приложению

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
+    cache: new InMemoryCache()
+});
+
+
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ApolloProvider>
 ), document.getElementById('root'));
 registerServiceWorker();
